10baea4d587cf290133422ef43a4fd3758ad4102
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-12-18 11:30:59 -0700

    Updated Navbar
    Imported icons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,20 @@
 import React from 'react'
 import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs"
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
 
 const Navbar = () => {
   return (
