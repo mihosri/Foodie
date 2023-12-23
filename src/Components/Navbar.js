7c531f3b4d33a35e136e65e484726f86b222a1bd
mihosri <srineethi.gurumurthigirahhalakshmi@edu.sait.ca>
2023-12-23 15:43:51 -0700

    Update in Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -44,7 +44,21 @@ const Navbar = () => {
 
   return (
     <nav>
-      
+      <div className="nav-logo-container">
+        <img src={Logo} />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className='navbar-cart-icon'/>
+        </a>
+        <button className='primary-button'>
+          Bookings Now
+        </button>
+      </div>
     </nav>
   )
 }
